f8cc595f5f36e09f84d164e2c1544d1186201d88
Mubassim Ahmed Khan <mubassimkhan@gmail.com>
2024-01-21 20:27:46 +0500

    27 - useSelector to render the state's value.

--- react-redux/src/components/cakeView.jsx
+++ react-redux/src/components/cakeView.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+// useSelector hook is used to get hold of any state that is mantained in Redux Store.
+import { useSelector } from "react-redux";
 
 export const CakeView = () => {
+    // useSelector accepts function as its parameter & that function is called selector function.
+    // Selector function accepts state as its arrgument.
+    const numOfCakes = useSelector((state) => state.cake.numOfCakes)
     return (
         <div>
-            <h2>Number of Cakes</h2>
+            <h2>Number of Cakes - {numOfCakes}</h2>
             <button>Order Cake</button>
             <button>Restock Cakes</button>
         </div>
--- react-redux/src/components/icecreamView.jsx
+++ react-redux/src/components/icecreamView.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+// useSelector hook is used to get hold of any state that is mantained in Redux Store.
+import { useSelector } from "react-redux";
 
 export const IcecreamView = () => {
+    // useSelector accepts function as its parameter & that function is called selector function.
+    // Selector function accepts state as its arrgument.
+    const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams)
     return (
         <div>
-            <h2>Number of Ice-creams</h2>
-            <button>Order Ice-cream</button>
-            <button>Restock Ice-creams</button>
+            <h2>Number of Ice creams - {numOfIcecreams} </h2>
+            <button>Order Ice cream</button>
+            <button>Restock Ice creams</button>
         </div>
     )
 }
