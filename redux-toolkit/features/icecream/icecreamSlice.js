9f42616efb3ac9a59d25e76cb42fe9fcbedab0ab
Mubassim Ahmed Khan <mubassimkhan@gmail.com>
2024-01-21 16:02:57 +0500

    23 - Used extra reducers to respond to other action types

--- redux-toolkit/features/icecream/icecreamSlice.js
+++ redux-toolkit/features/icecream/icecreamSlice.js
@@ -1,3 +1,5 @@
+const { cakeActions } = require('../cake/cakeSlice')
+
 const createSlice  = require('@reduxjs/toolkit').createSlice
 
 const initialState = {
@@ -14,6 +16,27 @@ const icecreamSlice = createSlice({
         restocked: (state, action) => {
             state.numOfIcecreams += action.payload
         }
+    },
+    // Use
+    /* Lets supose scenario where a free iceceam is given to every customer who buys a cake. In redux-toolkit we will use extraReducers which will help us in changing the state of other slices.
+    * There are 2 ways to write this 
+    
+    * First way (Removed from redux-toolkit) (Error: The object notation for `createSlice.extraReducers` has been removed. Please use the 'builder callback' notation instead) */
+    // extraReducers: {
+    //     ['cake/ordered']: (state) => {
+    //         state.numOfIcecreams --
+    //     }
+    // } 
+
+    // Second way (Recommended way)
+    /* 
+        We use extraReducers as a function which recieves arrgument 'builder'
+        Within function body we use "builder" to add a case (addCase). The first arrgument is action type, and then state & payload which are typical reducer type of functio, to mutate the state. 
+    */
+    extraReducers: (builder) => {
+        builder.addCase(cakeActions.ordered, state => {
+            state.numOfIcecreams--
+        })
     }
 })
 
