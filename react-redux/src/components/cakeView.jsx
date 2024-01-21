61113ec698cda48790a4a243c378655369db9938
Mubassim Ahmed Khan <mubassimkhan@gmail.com>
2024-01-21 20:45:54 +0500

    28 - useDispatch to dispatch the actions from React component.

--- react-redux/src/components/cakeView.jsx
+++ react-redux/src/components/cakeView.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 // useSelector hook is used to get hold of any state that is mantained in Redux Store.
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+// Import Action Creators from cakeSlice
+import { ordered, restocked } from '../features/cake/cakeSlice';
 
 export const CakeView = () => {
     // useSelector accepts function as its parameter & that function is called selector function.
     // Selector function accepts state as its arrgument.
     const numOfCakes = useSelector((state) => state.cake.numOfCakes)
+
+    // useDispatch returns a reference to the dispatch function from redux store
+    const dispatch = useDispatch();
+
+    // useState to customize the restock value
+    const [value, setValue] = useState(1);
     return (
         <div>
             <h2>Number of Cakes - {numOfCakes}</h2>
-            <button>Order Cake</button>
-            <button>Restock Cakes</button>
+            <button onClick={() => dispatch(ordered())}>Order Cake</button>
+            <input
+                type="number"
+                value={value}
+                onChange={(e) => setValue(parseInt(e.target.value))}
+            />
+            {/* To put restock value user input. The value of input element does not have to be part of Redux store, it can be a part of local state. */}
+            <button onClick={() => dispatch(restocked(value))}>Restock Cakes</button>
         </div>
     )
 }
--- react-redux/src/components/icecreamView.jsx
+++ react-redux/src/components/icecreamView.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 // useSelector hook is used to get hold of any state that is mantained in Redux Store.
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+// Import Action Creators from cakeSlice
+import { ordered, restocked } from '../features/icecream/icecreamSlice';
 
 export const IcecreamView = () => {
     // useSelector accepts function as its parameter & that function is called selector function.
     // Selector function accepts state as its arrgument.
     const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams)
+
+    // useDispatch returns a reference to the dispatch function from redux store
+    const dispatch = useDispatch()
+
+    // useState to customize the restock value
+    const [value, setValue] = useState(1);
+
     return (
         <div>
             <h2>Number of Ice creams - {numOfIcecreams} </h2>
-            <button>Order Ice cream</button>
-            <button>Restock Ice creams</button>
+            <button onClick={() => dispatch(ordered())}>Order Ice cream</button>
+            {/* To put restock value user input. The value of input element does not have to be part of Redux store, it can be a part of local state. */}
+            <input type="number"
+                value={value}
+                onChange={(e) => setValue(parseInt(e.target.value))}
+            />
+            <button onClick={() => dispatch(restocked(value))}>Restock Ice creams</button>
         </div>
     )
 }
